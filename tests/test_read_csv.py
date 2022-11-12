bcb3740f85f71a679390045a65b1122c1922de83
Matt Harris <maharris@mattharris-laptop.dyndns.cern.ch>
2022-11-12 12:37:48 +0100

    Moved the test for datapoints.py to the correct spot and verified that pytest could find the file

--- tests/test_read_csv.py
+++ tests/test_read_csv.py
@@ -1,9 +1,8 @@
 import os
-import pytest
-import Core.datapoints as dp
+import numpy as np
+from src.datapoints import datapoints as dp
 #use pytest to run this test
 
-@pytest.fixture
 def test_read_csv():
     # Create a test file of depth 5, first using the full names of the columns
     with open('test.csv', 'w') as f:
@@ -15,9 +14,42 @@ def test_read_csv():
         f.write('25,26,27,28,29,30\n')
         f.close()
 
+    #now we will load in the csv file and check it gives the expected values
+    
+    points = dp(0,0,0)
+    points.read_csv('test.csv')
+    assert(len(points.latitude) != 0)
+    assert(len(points.longitude) != 0)
+    assert(len(points.timestamp) != 0)
+    assert(len(points.latitude) == len(points.longitude) == len(points.timestamp))
+    assert(np.array_equal(points.latitude, np.array([1,7,13,19,25])))
+    assert(np.array_equal(points.longitude, np.array([2,8,14,20,26])))
+    assert(np.array_equal(points.timestamp, np.array([3,9,15,21,27])))
+
+    os.remove('test.csv')
+
+    #now we do the same thing but using the short names
+    with open('test.csv', 'w') as f:
+        f.write('lat,long,time,random1,random2,random3\n') 
+        f.write('1,2,3,4,5,6\n')
+        f.write('7,8,9,10,11,12\n')
+        f.write('13,14,15,16,17,18\n')
+        f.write('19,20,21,22,23,24\n')
+        f.write('25,26,27,28,29,30\n')
+        f.close()
+
         #now we will load in the csv file and check it gives the expected values
-        
-    dp.datapoints.read_csv(dp.datapoints, 'test.csv')
-    assert(dp.datapoints.latitude == [1,7,13,19,25])
-    assert dp.datapoints.longitude == [2,8,14,20,26]
-    assert dp.datapoints.timestamp == [3,9,15,21,27]
+
+    points = dp(0,0,0)
+    points.read_csv('test.csv')
+    assert(len(points.latitude) != 0)
+    assert(len(points.longitude) != 0)
+    assert(len(points.timestamp) != 0)
+    assert(len(points.latitude) == len(points.longitude) == len(points.timestamp))
+    assert(np.array_equal(points.latitude, np.array([1,7,13,19,25])))
+    assert(np.array_equal(points.longitude, np.array([2,8,14,20,26])))
+    assert(np.array_equal(points.timestamp, np.array([3,9,15,21,27])))
+
+    os.remove('test.csv')
+    '''I don't think a test is needed for reading in the .xsl files since it is an identical test to the .csv files,
+    But i can add them in if anyone thinks it is needed'''
