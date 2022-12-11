84fbc7f1487803e7368c2114ef46a7325554af98
skkwan <kystephkwan2@gmail.com>
2022-12-11 20:39:11 +0100

    add multiGeoVisualizer class for process_csv input

---
+++ src/multiGeoVisualizer.py
@@ -0,0 +1,43 @@
+from dataclasses import dataclass
+from typing import List
+
+import geopandas
+import matplotlib.pyplot as plt
+import pandas
+from geopandas import GeoDataFrame
+from shapely.geometry import Point
+
+from geoVisualizer import geoVisualizer
+from process_csv import process_csv
+
+@dataclass
+class multiGeoVisualizer:
+    nTracks: int
+    geoVisList: List[geoVisualizer]
+
+    def __init__(self, inputTracks: process_csv):
+        """Initialize a multiGeoVisualizer object using the process_csv class as input."""
+
+        if (len(inputTracks) == 0):
+            raise Exception(
+                "Input list has no elements!"
+            )
+        
+        self.nTracks = len(inputTracks)
+        print("Setting nTracks to", self.nTracks)
+
+        self.geoVisList = [geoVisualizer(track) for track in inputTracks]
+
+    def plot(self):
+        """Plot all available tracks to different files
+
+        Returns
+        ------
+        A list of the output plot names.
+        """
+        plotNames = []
+        for i in range(self.nTracks):
+            plotName = "plotTrack_{number}.pdf".format(number = i)
+            self.geoVisList[i].plot(plotName)
+            plotNames.append(plotName)
+        return plotNames
---
+++ test/test_multiGeoVisualizer.py
@@ -0,0 +1,25 @@
+import os
+
+from src.process_csv import process_csv
+from src.multiGeoVisualizer import multiGeoVisualizer
+
+def test_multiGeoVisualizer():
+    """Test array-based input."""
+    with open("test.csv", "w") as f:
+        f.write("latitude,longitude,timestamp,random1,random2,random3\n")
+        f.write("1,2,3,4,5,6\n")
+        f.write("7,8,9,10,11,12\n")
+        f.write("13,14,3,16,17,18\n")
+        f.write("19,20,9,22,23,24\n")
+        f.write("25,26,3,28,29,30\n")
+        f.close()
+
+    separated_data = process_csv().process("test.csv")
+    assert len(separated_data) == 3
+
+    plotNames = multiGeoVisualizer(separated_data).plot()
+    for plotName in plotNames:
+        assert os.path.exists(plotName)
+        os.remove(plotName)
+    
+    os.remove("test.csv")
